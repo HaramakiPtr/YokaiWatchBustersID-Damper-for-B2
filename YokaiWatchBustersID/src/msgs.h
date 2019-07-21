d921932c2228d40f3c0024a2304a017c65037917
haramaki[ptr] <49096527+HaramakiPtr@users.noreply.github.com>
2019-07-22 00:08:45 +0900

    Add files via upload

---
+++ YokaiWatchBustersID/src/msgs.h
@@ -0,0 +1,16 @@
+#ifndef MSGS_H
+#define MSGS_H
+
+static const char* const MSGS[] =
+{
+	"CAN NOT OPEN THE FILE!\n",
+	"ファイルが違います。\n",
+	"解析には「妖怪ウォッチバスターズ」のRAMダンプファイル",
+	" 0x08000000 ～ 0x08F81000 ",
+	"が必要です。\n",
+	"FILE HAS BEEN OPENED!\n",
+	"------------------\n",
+	"妖怪ウォッチバスターズ妖怪ID抽出プログラム\nby:haramaki[ptr]\n"
+};
+
+#endif
